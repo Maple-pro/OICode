06c2a612009feb63f32b0084fc151baa13a391a9
chengfeng <1719957182@qq.com>
2020-11-26 01:10:03 +0800

    fix bug

--- Codeforces/Codeforces #618 (Div.2)/b.cpp
+++ Codeforces/Codeforces #618 (Div.2)/b.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 const int maxn = 1e5 + 5;
-int data[2*maxn];
+int datas[2*maxn];
 
 int main()
 {
@@ -13,11 +13,11 @@ int main()
 		int n;
 		cin >> n;
 		for (int i=1; i<=2*n; i++){
-			scanf("%d", &data[i]);
+			scanf("%d", &datas[i]);
 		}
 
-		sort(data+1, data+n+1);
-		int result = data[n+1] - data[n];
+		sort(datas+1, datas+n+1);
+		int result = datas[n+1] - datas[n];
 
 		printf("%d\n", result);
 	}
